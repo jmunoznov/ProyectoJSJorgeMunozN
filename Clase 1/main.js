e57c4a1360dc837f455fad0dcd9593122a5b999f
Jorge Muñoz Novakovic <jmunoznov@gmail.com>
2023-09-20 23:36:12 -0300

    Git Ignore

---
+++ Clase 1/main.js
@@ -0,0 +1,16 @@
+/* Alt + Shift + A | en Windows */
+/*
+let nombre = prompt("Ingresar un nombre");
+console.log("Hola ",nombre, ", bienvenido!");
+
+let numeroSistema = 7;
+let numeroUser = parseInt(prompt("Ingrese un nro"));
+let suma = numeroSistema + numeroUser;
+console.log(numeroSistema + " + " + numeroUser + " = " + suma);
+
+let primerTexto = prompt("Ingresar un primer texto");
+let segundoTexto = prompt("Ingresar un segundo texto");
+let textoConcaternado = primerTexto + " " + segundoTexto;
+console.log(textoConcaternado);
+
+*/
